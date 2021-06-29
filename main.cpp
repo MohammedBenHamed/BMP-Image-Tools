8b9a5bae37e2ed9532dbd45a625858c759bf31c7
MohammedBenHamed <mohammedbenhamed@outlook.com>
2021-06-29 12:34:05 +0100

    Updated main fie

--- main.cpp
+++ main.cpp
@@ -1,34 +1,19 @@
-#define __USE_MINGW_ANSI_STDIO 0 // std::to_string()
 #include <iostream>
-#include "Functions/Tool.h"
 #include "Pic/Pixel.h"
 #include "Pic/Img.h"
 #include "File/File.h"
-#include "Functions/Operation.h"
-#include <fstream>
-#include <sstream>
-#include <stdexcept>
+#include "File/ImageFile.h"
+#include "Operation/Operation.h"
+#include "IO/MainLoop.h"
+#include "IO/Script.h"
+
 
 int main()
 {
+    consoleIO cio;
+    cio.run();
 
-    std::vector<Img> iList;
-    try
-    {
-        iList.push_back(File::openImage("test.bmp"));
-    }
-    catch (const std::out_of_range& oor)
-    {
-        std::cout << "File could not be found. Aborting...\n";
-        return -1;
-    }
-
-    Operation::cropEdges(iList.at(0),Pixel(255,255,255));
-    File::WriteAs::BMP24(iList.at(0),"output.bmp");
 
     return 0;
-
-
-
 
 }
